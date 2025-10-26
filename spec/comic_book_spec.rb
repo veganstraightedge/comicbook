386298f3fe667d6513f7d51d1daef9fedddaa814
Shane Becker <veganstraightedge@gmail.com>
2025-10-26 16:12:29 -0700

    Refactor to RSpec patterns

--- spec/comic_book_spec.rb
+++ spec/comic_book_spec.rb
@@ -20,50 +20,60 @@ RSpec.describe ComicBook do
   end
 
   describe '#new' do
-    it 'creates a ComicBook instance with a file path' do
-      cb = described_class.new test_file
-      test_file_path = File.expand_path(test_file)
+    context 'with a file path' do
+      subject(:cb) { described_class.new test_file }
 
-      expect(cb).to be_a described_class
-      expect(cb.path).to eq test_file_path
-      expect(cb.type).to eq :cbz
+      let(:test_file_path) { File.expand_path(test_file) }
+
+      it 'creates a ComicBook instance with a file path' do
+        expect(cb).to be_a described_class
+        expect(cb.path).to eq test_file_path
+        expect(cb.type).to eq :cbz
+      end
     end
 
-    it 'creates a ComicBook instance with a folder path' do
-      cb = described_class.new test_folder
-      test_folder_path = File.expand_path(test_folder)
+    context 'with a folder path' do
+      subject(:cb) { described_class.new test_folder }
 
-      expect(cb).to be_a described_class
-      expect(cb.path).to eq test_folder_path
-      expect(cb.type).to eq :folder
+      let(:test_folder_path) { File.expand_path(test_folder) }
+
+      it 'creates a ComicBook instance with a folder path' do
+        expect(cb).to be_a described_class
+        expect(cb.path).to eq test_folder_path
+        expect(cb.type).to eq :folder
+      end
     end
 
-    it 'raises error for non-existent path' do
-      expect { described_class.new('/non/existent/path') }.to raise_error ComicBook::Error, /Path does not exist/
+    context 'with a non-existent path' do
+      it 'raises an error' do
+        expect { described_class.new('/non/existent/path') }.to raise_error ComicBook::Error, /Path does not exist/
+      end
     end
 
-    it 'raises error for unsupported file type' do
-      unsupported_file = File.join temp_dir, 'test.txt'
-      File.write unsupported_file, 'content'
+    context 'with an unsupported file type' do
+      let(:unsupported_file) { File.join temp_dir, 'test.txt' }
 
-      expect { described_class.new(unsupported_file) }.to raise_error(ComicBook::Error, /Unsupported file type/)
+      it 'raises an error' do
+        File.write unsupported_file, 'content'
+
+        expect { described_class.new(unsupported_file) }.to raise_error ComicBook::Error, /Unsupported file type/
+      end
     end
   end
 
   describe '.load' do
-    it 'returns a ComicBook instance' do
-      cb = described_class.load test_file
+    subject(:cb) { described_class.load test_file }
 
+    it 'returns a ComicBook instance' do
       expect(cb).to be_a described_class
       expect(cb.path).to eq File.expand_path(test_file)
     end
 
     it 'is equivalent to .new' do
-      cb_new  = described_class.new  test_file
-      cb_load = described_class.load test_file
+      cb_via_new = described_class.new test_file
 
-      expect(cb_new.path).to eq cb_load.path
-      expect(cb_new.type).to eq cb_load.type
+      expect(cb_via_new.path).to eq cb.path
+      expect(cb_via_new.type).to eq cb.type
     end
   end
 
