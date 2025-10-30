a581caa4d9d5e3479e8593565ce51d57fbb080ee
Shane Becker <veganstraightedge@gmail.com>
2025-10-30 01:07:23 -0700

    Seattle.rb style

--- lib/comic_book/cbz.rb
+++ lib/comic_book/cbz.rb
@@ -28,17 +28,17 @@ class ComicBook
         end
       end
 
-      pages.sort_by(&:name)
+      pages.sort_by &:name
     end
 
     def create_page_from_entry entry
-      basename = File.basename(entry.name)
+      basename = File.basename entry.name
 
       ComicBook::Page.new entry.name, basename
     end
 
     def image_file? filename
-      extension = File.extname(filename.downcase)
+      extension = File.extname filename.downcase
 
       ComicBook::IMAGE_EXTENSIONS.include? extension
     end
