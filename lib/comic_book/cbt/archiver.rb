e715e07fd05057d6a879ac8e6832a187d600e6f6
Shane Becker <veganstraightedge@gmail.com>
2026-01-17 02:48:36 -0800

    Standardize method and arg names

--- lib/comic_book/cbt/archiver.rb
+++ lib/comic_book/cbt/archiver.rb
@@ -7,13 +7,13 @@ class ComicBook
 
       def archive options = {}
         extension       = options.fetch :extension, :cbt
-        destination     = options[:to] || determine_output_path(extension)
         delete_original = options.fetch :delete_original, false
 
-        create_tar_file destination
+        output_path = options[:to] || determine_output_path(extension)
+        create_archive output_path
         cleanup_source_folder if delete_original
 
-        destination
+        output_path
       end
 
       private
@@ -27,41 +27,32 @@ class ComicBook
         File.expand_path File.join(dir_name, "#{base_name}.#{extension}")
       end
 
-      def create_tar_file destination
-        File.open(destination, 'wb') do |file|
-          Gem::Package::TarWriter.new(file) do |tar|
-            add_files_to_tar tar, source_folder
+      def create_archive output_path
+        File.open(output_path, 'wb') do |file|
+          Gem::Package::TarWriter.new(file) do |writer|
+            find_image_files.each do |image_file|
+              add_file writer, image_file
+            end
           end
         end
       end
 
-      def add_files_to_tar tar, folder, prefix = ''
-        Dir.entries(folder).sort.each do |entry|
-          next if ['.', '..'].include?(entry)
-
-          full_path = File.join(folder, entry)
-          tar_path = prefix.empty? ? entry : File.join(prefix, entry)
-
-          if File.directory?(full_path)
-            add_files_to_tar tar, full_path, tar_path
-          elsif image_file?(entry)
-            add_file_to_tar tar, full_path, tar_path
-          end
-        end
+      def find_image_files
+        pattern = File.join(source_folder, '**', ComicBook::IMAGE_GLOB_PATTERN)
+        Dir.glob(pattern, File::FNM_CASEFOLD).sort
       end
 
-      def add_file_to_tar tar, file_path, tar_path
-        stat = File.stat(file_path)
-        tar.add_file(tar_path, stat.mode) do |io|
-          File.open(file_path, 'rb') do |file|
-            io.write(file.read)
+      def add_file writer, file
+        file_path     = Pathname.new file
+        source_path   = Pathname.new source_folder
+        relative_path = file_path.relative_path_from source_path
+
+        stat = File.stat file
+        writer.add_file(relative_path.to_s, stat.mode) do |io|
+          File.open(file, 'rb') do |f|
+            io.write f.read
           end
         end
-      end
-
-      def image_file? filename
-        extension = File.extname(filename.downcase)
-        ComicBook::IMAGE_EXTENSIONS.include? extension
       end
 
       def cleanup_source_folder
